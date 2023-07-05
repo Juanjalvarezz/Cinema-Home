0ed6b42163fb839a6d82344e17815fe57aa9d4f8
CrimvaelVR <maikelvr2004@gmail.com>
2023-07-05 05:13:55 -0400

    Ahora tiene paginacion NEXT-PREV
    Además tmb tiene debounce presuntamente

--- juan/script.js
+++ juan/script.js
@@ -1,106 +1,157 @@
+const API_URL = "https://api.themoviedb.org/3";
+const API_KEY = "4c7644be5749ff57cb3cd3e80f76d300";
+let currentPage = 1;
 
-function buscarPelis(query) {
-    $.ajax({
-      url: `https://api.themoviedb.org/3/search/movie?api_key=4c7644be5749ff57cb3cd3e80f76d300&language=es-ES&query=${query}`,
-      success: function(data) {
-        // Crea una lista de películas coincidentes
-        let html = `
-          <ul class="movie-list">
-        `;
-        data.results.forEach(movie => {
-          html += `
-            <li class="movie-info contenedor">
-              <img src="https://image.tmdb.org/t/p/w185/${movie.poster_path}"/>
-              <div class="pad">
-                <h3>${movie.title}</h3>
-                <p>${movie.overview}</p>
-                <p><b>Fecha de lanzamiento:</b> ${movie.release_date}</p>
-                <button class="view-more boton" data-movie-id="${movie.id}">Ver más</button>
-              </div>
-            </li>
-          `;
-        });
-        html += `
-          </ul>
-        `;
-        // Agrega la lista de películas a la página
-        $("#movies").empty().append(html);
-      }
-    });
-  }
-  
-  function detallesPeli(IDPeli) {
-    $.ajax({
-      url: `https://api.themoviedb.org/3/movie/${IDPeli}?api_key=4c7644be5749ff57cb3cd3e80f76d300&language=es-ES&append_to_response=videos,credits`,
-      success: function(data) {
-        // Con esto creamos una vista detallada de la peli, con la estructura que tendría un HTML
-        let html = `
-          <div class="movie-detail pad">
-            <img src="https://image.tmdb.org/t/p/w500/${data.poster_path}" />
-            <h2>${data.title}</h2>
-            <p>${data.overview}</p>
-            <p><b>Género:</b> ${data.genres.map(g => g.name).join(", ")}</p>
-            <p><b>Fecha de lanzamiento:</b> ${data.release_date}</p>
-            <p><b>Puntuación:</b> ${data.vote_average}</p>
-            <h2>Actores principales</h2>
-            <div class="cast">
-        `;
-        // Con esto buscamos el cast, imagen del actor y su personaje en la peli
-        let cast = data.credits.cast.slice(0, 5);
-        cast.forEach(actor => {
-          html += `
-            <div class="actor">
-              <img src="https://image.tmdb.org/t/p/w185/${actor.profile_path}" />
-              <p>${actor.name} (${actor.character})</p>
-            </div>
-          `;
-        }); ////Con esto buscamos al director e imagen del mismo
-        let director = data.credits.crew.find(person => person.job === "Director");
-        if (director) {
-          html += `
-            <div class="actor">
-              <img src="https://image.tmdb.org/t/p/w185/${director.profile_path}" />
-              <p>${director.name} (Director)</p>
-            </div>
-          `;
-        }
-        html += `
-            </div>
-            <h2>Videos</h2>
-            <ul>
-        `;
-        // Con esto tenemos para buscar 3 videos como maximo de pelis.
-        let videos = data.videos.results.slice(0, 3);
-        videos.forEach(video => {
-          html += `<li class="boton"><a href="https://www.youtube.com/watch?v=${video.key}" target="_blank">${video.name}</a></li>`;
-        });
-        html += `
-            </ul>
-          </div>
-        `;
-        // Borra todo el contenido con un empty y luego manda el nuevo con un append
-        $("#movies").empty().append(html);
-      }
-    });
-  }
-  
-  // Apenas se llena el formulario y le das submit se buscaran las peliculas aplicando la primera función
-  $("#search-form").submit(function(event) { 
-    event.preventDefault();
-    let query = $("#query").val().trim();
-    if (query !== "") {
-      buscarPelis(query);
+document.addEventListener("DOMContentLoaded", function() {
+  const debouncedCargasPelis = debounce(cargasPelis, 500);
+
+  debouncedCargasPelis(currentPage);
+
+  document.querySelector("#prev-page").addEventListener("click", function() {
+    if (currentPage > 1) {
+      currentPage--;
+      debouncedCargasPelis(currentPage);
     }
   });
-  
-  // Cuando se hace clic en "Ver más" en una película, se obtiene información detallada de esa película
-  $(document).on("click", ".view-more", function(event) {
-    event.preventDefault();
-    let IDPeli = $(this).data("movie-id");
-    detallesPeli(IDPeli);
-  });
 
-//Boton top
+  document.querySelector("#next-page").addEventListener("click", function() {
+    currentPage++;
+    debouncedCargasPelis(currentPage);
+  });
+});
+
+function cargasPelis(page) {
+  fetch(`${API_URL}/movie/popular?api_key=${API_KEY}&language=es-ES&page=${page}`)
+  .then(response => response.json())
+  .then(data => {
+    let moviesContainer = document.querySelector("#movies");
+    moviesContainer.innerHTML = ""; // Clear previous movies from container
+    for (let i = 0; i < data.results.length; i++) {
+      let movie = data.results[i];
+      let movieDiv = document.createElement("div");
+      movieDiv.classList.add("movie");
+      let img = document.createElement("img");
+      img.src = `https://image.tmdb.org/t/p/w500/${movie.poster_path}`;
+      let h2 = document.createElement("h2");
+      h2.textContent = movie.title;
+      movieDiv.appendChild(img);
+      movieDiv.appendChild(h2);
+      moviesContainer.appendChild(movieDiv);
+    }
+  })
+  .catch(error => console.log(error));
+}
+
+function buscarPelis(query) {
+  fetch(`${API_URL}/search/movie?api_key=${API_KEY}&language=es-ES&query=${query}`)
+    .then(response => response.json())
+    .then(data => {
+      let html = `
+        <ul class="movie-list">
+      `;
+      data.results.forEach(movie => {
+        html += `
+          <li class="movie-info contenedor">
+            <img src="https://image.tmdb.org/t/p/w185/${movie.poster_path}"/>
+            <div class="pad">
+              <h3>${movie.title}</h3>
+              <p>${movie.overview}</p>
+              <p>Fecha de lanzamiento: ${movie.release_date}</p>
+              <button class="view-more boton" data-movie-id="${movie.id}">Ver más</button>
+            </div>
+          </li>
+        `;
+      });
+      html += `
+        </ul>
+      `;
+      $("#movies").empty().append(html);
+    })
+    .catch(error => console.log(error));
+}
+
+function debounce(func, timeout = 5000) {
+  let timer;
+  return (...args) => {
+    clearTimeout(timer);
+    timer = setTimeout(() => {
+      func.apply(this, args);
+    }, timeout);
+  };
+}
+
+const buscarPelisDebounced = debounce(buscarPelis, 5000);
+
+$("#search").on("keyup", function() {
+  const query = $(this).val();
+  buscarPelisDebounced(query);
+});
+
+function detallesPeli(IDPeli) {
+  fetch(`${API_URL}/movie/${IDPeli}?api_key=${API_KEY}&language=es-ES&append_to_response=videos,credits`)
+    .then(response => response.json())
+    .then(data => {
+      let html = `
+        <div class="movie-detail">
+          <img src="https://image.tmdb.org/t/p/w500/${data.poster_path}" />
+          <h2>${data.title}</h2>
+          <p>${data.overview}</p>
+          <p><b>Género:</b> ${data.genres.map(g => g.name).join(", ")}</p>
+          <p><b>Fecha de lanzamiento:</b> ${data.release_date}</p>
+          <p><b>Puntuación:</b> ${data.vote_average}</p>
+          <h2>Actores principales</h2>
+          <div class="cast">
+      `;
+      let cast = data.credits.cast.slice(0, 5);
+      cast.forEach(actor => {
+        html += `
+          <div class="actor">
+            <img src="https://image.tmdb.org/t/p/w185/${actor.profile_path}" />
+            <p>${actor.name} (${actor.character})</p>
+          </div>
+        `;
+      });
+      let director = data.credits.crew.find(person => person.job === "Director");
+      if (director) {
+        html += `
+          <div class="actor">
+            <img src="https://image.tmdb.org/t/p/w185/${director.profile_path}" />
+            <p>${director.name} (Director)</p>
+          </div>
+        `;
+      }
+      html += `
+          </div>
+          <h2>Videos</h2>
+          <ul>
+      `;
+      let videos = data.videos.results.slice(0, 3);
+      videos.forEach(video => {
+        html += `<li class="boton"><a href="https://www.youtube.com/watch?v=${video.key}" target="_blank">${video.name}</a></li>`;
+      });
+      html += `
+          </ul>
+        </div>
+      `;
+      $("#movies").empty().append(html);
+    })
+    .catch(error => console.log(error));
+}
+
+$("#search-form").submit(function(event) { 
+  event.preventDefault();
+  let query = $("#query").val().trim();
+  if (query !== "") {
+    buscarPelis(query);
+  }
+});
+
+$(document).on("click", ".view-more", function(event) {
+  event.preventDefault();
+  let IDPeli = $(this).data("movie-id");
+  detallesPeli(IDPeli);
+});
+
 let mybutton = document.getElementById("myBtn");
 
 window.onscroll = function() {scrollFunction()};
@@ -117,6 +168,7 @@ function topFunction() {
   document.body.scrollTop = 0; 
   document.documentElement.scrollTop = 0; 
 }
+
 
 //Loader
 window.onload = function(){
